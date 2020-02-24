52169f82908298baeb610546558994ce6d79ee3d
DanielRivasMD <danielrivasmd@gmail.com>
2020-02-24 15:05:36 +0100

    use switch go

--- Script/GoWorkshop/assembly_stats/fieldExtractorF.go
+++ Script/GoWorkshop/assembly_stats/fieldExtractorF.go
@@ -15,44 +15,37 @@ func fieldExtractor(scanFile *os.File, toPrintRecord map[string]string) map[stri
 
 		records := strings.Split(scanner.Text(), "\t")
 
-		// TODO: use switch instead of if
+		switch {
 		// Assembly name
-		if strings.Contains(records[0], "Assembly name") {
+		case strings.Contains(records[0], "Assembly name"):
 			toPrintRecord["Assembly"] = spaceTrimmer(splitColon(records[0]))
-		}
 
 		// Assembly level
-		if strings.Contains(records[0], "Assembly level") {
+		case strings.Contains(records[0], "Assembly level"):
 			toPrintRecord["Level"] = spaceTrimmer(splitColon(records[0]))
-		}
 
 		// Organism name
-		if strings.Contains(records[0], "Organism name") {
+		case strings.Contains(records[0], "Organism name"):
 			toPrintRecord["Organism"] = spaceTrimmer(splitColon(records[0]))
-		}
 
 		// total-length
-		if len(records) >= 5 && records[0] == "all" && records[4] == "total-length" {
+		case len(records) >= 5 && records[0] == "all" && records[4] == "total-length":
 			toPrintRecord["TotalLength"] = records[5]
-		}
 
 		// scaffold-N50
-		if len(records) >= 5 && records[0] == "all" && records[4] == "scaffold-N50" {
+		case len(records) >= 5 && records[0] == "all" && records[4] == "scaffold-N50":
 			toPrintRecord["ScaffoldN50"] = records[5]
-		}
 
 		// scaffold-count
-		if len(records) >= 5 && records[0] == "all" && records[4] == "scaffold-count" {
+		case len(records) >= 5 && records[0] == "all" && records[4] == "scaffold-count":
 			toPrintRecord["ScaffoldCount"] = records[5]
-		}
 
 		// contig-N50
-		if len(records) >= 5 && records[0] == "all" && records[4] == "contig-N50" {
+		case len(records) >= 5 && records[0] == "all" && records[4] == "contig-N50":
 			toPrintRecord["ContigN50"] = records[5]
-		}
 
 		// contig-count
-		if len(records) >= 5 && records[0] == "all" && records[4] == "contig-count" {
+		case len(records) >= 5 && records[0] == "all" && records[4] == "contig-count":
 			toPrintRecord["ContigCount"] = records[5]
 		}
 	}
