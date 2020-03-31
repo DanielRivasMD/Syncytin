4763ccc1176aba2287f8d8782756d528037779d5
DanielRivasMD <danielrivasmd@gmail.com>
2020-03-31 15:44:41 +0200

    taxonommy classifer v2.1. extraction from DNA zoo sra experiments

--- Script/taxonomyClassifier/taxonomy_DNAzoo.jl
+++ Script/taxonomyClassifier/taxonomy_DNAzoo.jl
@@ -18,6 +18,7 @@ script_dir = "Script/"
 data_dir = "Data/"
 dnazoo_dir = "DNAzoo/"
 example = "ex1.xml"
+dnazoo_xml = "SraExperimentPackage.xml"
 
 ################################################################################
 
@@ -40,5 +41,40 @@ for c in child_nodes(xroot)  # c is an instance of XMLNode
 end
 
 ces = collect(child_elements(xroot))  # get a list of all child elements
+
+################################################################################
+
+# change directory
+cd(proj_dir)
+
+# read file
+xdoc = parse_file("$(data_dir)$(dnazoo_dir)$(dnazoo_xml)")
+
+# get the root element
+xroot = root(xdoc)  # an instance of XMLElement
+
+# create an empty dataframe
+tax_df = DataFrame(scientific_name = String[], taxon_id = String[])
+
+counter = 0
+for i in child_elements(xroot)
+  global counter
+  counter += 1
+  sam = find_element(i, "SAMPLE")
+  samnam = find_element(sam, "SAMPLE_NAME")
+  println()
+  @show counter
+  content(find_element(samnam, "TAXON_ID")) |> println
+  content(find_element(samnam, "SCIENTIFIC_NAME")) |> println
+  push!(
+    tax_df,
+    [
+      content(find_element(samnam, "SCIENTIFIC_NAME")),
+      content(find_element(samnam, "TAXON_ID"))
+    ]
+  )
+end
+
+freqtable(tax_df, :scientific_name) |> sort
 
 ################################################################################
