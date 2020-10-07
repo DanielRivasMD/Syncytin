547a6e9527361b607ab2d1ec592145d0c6219204
DanielRivasMD <danielrivasmd@gmail.com>
2020-10-07 16:00:09 +0200

    download blast & locate magnus

---
+++ src/ncbi-blast.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+################################################################################
+
+MAGNUS=magnus.pawsey.org.au
+
+################################################################################
+
+# create directory
+mkdir -v software
+cd software
+
+################################################################################
+
+# download blast toolkit
+wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.10.1+-x64-linux.tar.gz
+
+# untar
+tar -zxvf ncbi-blast-2.10.1+-x64-linux.tar.gz
+cd ncbi-blast-2.10.1+/bin
+
+# transfer
+scp makeblastdb ${MAGNUS}:bin/
+scp tblastn ${MAGNUS}:bin/
+
+################################################################################
