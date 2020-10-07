816b1c83f1a861669a971d0d2b7662c1f7d132d3
DanielRivasMD <danielrivasmd@gmail.com>
2020-10-08 00:31:10 +0200

    genome blast slurm controller

---
+++ src/SlurmController/genomeBlast.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+################################################################################
+
+projId=pawsey0263
+projDir=${HOME}/Factorem/Syncytin
+inputLs=${projDir}/data/assembly.list
+arNo=$( awk 'END{print NR}' ${inputLs} )
+
+################################################################################
+
+gbId=$( sbatch \
+    --account $projId \
+    --job-name SyncytinBlast \
+    --output ${HOME}/report/SyncytinBlast.output \
+    --error ${HOME}/report/SyncytinBlast.err \
+    --time 1:0:0 \
+    --nodes 1 \
+    --ntasks 1 \
+    --array 1-$arNo \
+    --export inputLs=$inputLs \
+  ${projDir}/src/genomeBlast.sh )
+
+
+################################################################################
