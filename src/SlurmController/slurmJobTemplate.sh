892676c3550f07bd1901ced9de5d34d1edeb3b2a
DanielRivasMD <danielrivasmd@gmail.com>
2021-02-13 16:39:57 +0100

    add slurm job template

---
+++ src/SlurmController/slurmJobTemplate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ${HOME}/Factorem/Syncytin/src/SlurmController/Syncytin_slurmConfig.sh
+jobId=WRITE_SLURM_JOBID_HERE
+
+################################################################################
+
+    # --clusters ${computingCluster} \
+slurmJobId=$( sbatch \
+    --account ${projectId} \
+    --job-name ${jobId} \
+    --output ${reportFolder}/${jobId}.out \
+    --error ${reportFolder}/${jobId}.err \
+    --time WRITE_TIME_HERE \
+    --nodes WRITE_NODES_HERE \
+    --ntasks WRITE_TASKS_HERE \
+    --array 1-${arNo} \
+    --export inputLs=${inputLs} \
+  ${sourceFolder}/WRITE_SCRIPT_PATH_HERE.sh )
+
+echo ${slurmJobId}
+
+################################################################################
+
