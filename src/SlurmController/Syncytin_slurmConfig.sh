96561516c1c9a94ee94009f0c9bac0a5feaa46ca
DanielRivasMD <danielrivasmd@gmail.com>
2021-02-13 16:39:41 +0100

    add slurm config

---
+++ src/SlurmController/Syncytin_slurmConfig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+################################################################################
+
+projId=pawsey0263
+sourceFolder=${HOME}/Factorem/Syncytin
+reportFolder=${HOME}/Report/Syncytin
+dataFolder=/crex/proj/snic2020-16-51/private/benchmark_Chapulin
+
+################################################################################
