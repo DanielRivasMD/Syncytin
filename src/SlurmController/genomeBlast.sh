75630e62621d63ed8fdedaaf6919cbbae2c6a218
DanielRivasMD <danielrivasmd@gmail.com>
2021-02-13 16:37:38 +0100

    update genome blast controllers

--- src/SlurmController/genomeBlast.sh
+++ src/SlurmController/genomeBlast.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-################################################################################
-
-projId=pawsey0263
-projDir=${HOME}/Factorem/Syncytin
-inputLs=${projDir}/data/assembly.list
-arNo=$( awk 'END{print NR}' ${inputLs} )
+source ${HOME}/Factorem/Syncytin/src/SlurmController/Syncytin_slurmConfig.sh
+jobId=SyncytinBlast
 
 ################################################################################
 
-gbId=$( sbatch \
-    --account $projId \
-    --job-name SyncytinBlast \
-    --output ${HOME}/report/SyncytinBlast.output \
-    --error ${HOME}/report/SyncytinBlast.err \
+slurmJobId=$( sbatch \
+    --account ${projectId} \
+    --job-name ${jobId} \
+    --output ${reportFolder}/${jobId}.out \
+    --error ${reportFolder}/${jobId}.err \
     --time 10:0:0 \
     --nodes 1 \
     --ntasks 1 \
-    --array 1-$arNo \
-    --export inputLs=$inputLs \
-  ${projDir}/src/blast/genomeBlast.sh )
+    --array 1-${arNo} \
+    --export inputLs=${inputLs} \
+  ${sourceFolder}/src/blast/genomeBlast.sh )
 
-echo ${gbId}
+echo ${slurmJobId}
 
 ################################################################################
+
--- src/blast/genomeBlast.sh
+++ src/blast/genomeBlast.sh
@@ -3,13 +3,13 @@
 ################################################################################
 
 # bash array
-bashArrIt=$(sed -n "$SLURM_ARRAY_TASK_ID"p $inputLs)
+bashArrIt=$(sed -n "$SLURM_ARRAY_TASK_ID"p ${inputLs})
 
 ################################################################################
 
-cd $HOME/Factorem/Syncytin
+cd ${HOME}/Factorem/Syncytin
 bender GenomeBlast \
-  --genome $bashArrIt \
+  --genome ${bashArrIt} \
   --library protein/syncytinLibrary.fasta
 
 ################################################################################
