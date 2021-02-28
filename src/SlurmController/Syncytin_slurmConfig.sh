f0625543ecb5ca3327c374058676d3be8422930b
DanielRivasMD <danielrivasmd@gmail.com>
2021-02-28 22:52:05 +0100

    update slurm config

--- src/SlurmController/Syncytin_slurmConfig.sh
+++ src/SlurmController/Syncytin_slurmConfig.sh
@@ -5,6 +5,6 @@
 projId=pawsey0263
 sourceFolder=${HOME}/Factorem/Syncytin
 reportFolder=${HOME}/Report/Syncytin
-dataFolder=/crex/proj/snic2020-16-51/private/benchmark_Chapulin
+inputLs=${sourceFolder}/data/assembly.list
 
 ################################################################################
