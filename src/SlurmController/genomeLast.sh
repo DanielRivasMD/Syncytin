341283782f6e17f04ca3212de9c2739945801766
DanielRivasMD <danielrivasmd@gmail.com>
2021-03-02 22:48:47 +0100

    patch genome last controller

--- src/SlurmController/genomeLast.sh
+++ src/SlurmController/genomeLast.sh
@@ -8,13 +8,14 @@ jobId=SyncytinLast
 
 slurmJobId=$( sbatch \
     --account ${projectId} \
-    --partition workq \
+    --partition gpuq \
     --job-name ${jobId} \
     --output ${reportFolder}/${jobId}.out \
     --error ${reportFolder}/${jobId}.err \
     --time 10:0:0 \
     --nodes 1 \
     --ntasks 1 \
+    --export sourceFolder=${sourceFolder} \
     --array 1-$( awk 'END{print NR}' ${sourceFolder}/data/assembly.list ) \
   --wrap 'bender --config ${sourceFolder}/src/last/genomeLast.toml GenomeLast --genome $(sed -n "$SLURM_ARRAY_TASK_ID"p /home/drivas/Factorem/Syncytin/data/assembly.list )' )
 
