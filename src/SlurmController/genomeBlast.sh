d742d44909a2e8d923026f60fe8230e86f69c044
DanielRivasMD <danielrivasmd@gmail.com>
2021-03-02 01:14:54 +0100

    update genome blast controller & deprecate sh cmd

--- src/SlurmController/genomeBlast.sh
+++ src/SlurmController/genomeBlast.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# NOTE: runs on Magnus for parallelization
 source ${HOME}/Factorem/Syncytin/src/SlurmController/Syncytin_slurmConfig.sh
 jobId=SyncytinBlast
 
@@ -7,17 +8,16 @@ jobId=SyncytinBlast
 
 slurmJobId=$( sbatch \
     --account ${projectId} \
+    --partition workq \
     --job-name ${jobId} \
     --output ${reportFolder}/${jobId}.out \
     --error ${reportFolder}/${jobId}.err \
     --time 10:0:0 \
     --nodes 1 \
     --ntasks 1 \
-    --array 1-${arNo} \
-    --export inputLs=${inputLs} \
-  ${sourceFolder}/src/blast/genomeBlast.sh )
+    --array 1-$( awk 'END{print NR}' ${sourceFolder}/data/assembly.list ) \
+  --wrap 'bender --config ${sourceFolder}/src/last/genomeBlast.toml GenomeBlast --genome $(sed -n "$SLURM_ARRAY_TASK_ID"p /home/drivas/Factorem/Syncytin/data/assembly.list )' )
 
 echo ${slurmJobId}
 
 ################################################################################
-
