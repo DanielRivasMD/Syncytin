710dfa0b68a49d59406770f8a9995a9c0f8dd3fc
DanielRivasMD <danielrivasmd@gmail.com>
2021-03-02 01:06:41 +0100

    patch slurm config. project id

--- src/SlurmController/Syncytin_slurmConfig.sh
+++ src/SlurmController/Syncytin_slurmConfig.sh
@@ -2,9 +2,8 @@
 
 ################################################################################
 
-projId=pawsey0263
+projectId=pawsey0263
 sourceFolder=${HOME}/Factorem/Syncytin
 reportFolder=${HOME}/Report/Syncytin
-inputLs=${sourceFolder}/data/assembly.list
 
 ################################################################################
