8aadfa77866f8c6a63716746d3d124b2b03495a5
DanielRivasMD <danielrivasmd@gmail.com>
2021-04-07 13:31:00 +0200

    add assembly collector controller

---
+++ src/SlurmController/assemblies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source ${HOME}/Factorem/Syncytin/src/SlurmController/Syncytin_slurmConfig.sh
+jobId=collector_HiC_assemblies
+
+################################################################################
+
+slurmJobId=$( sbatch \
+    --account ${projectId} \
+    --partition gpuq \
+    --job-name ${jobId} \
+    --output ${reportFolder}/${jobId}.out \
+    --error ${reportFolder}/${jobId}.err \
+    --time 24:00:00 \
+    --nodes 1 \
+    --ntasks 1 \
+  ${sourceFolder}/src/collector/assemblies.sh )
+
+echo ${slurmJobId}
+
+################################################################################
+
