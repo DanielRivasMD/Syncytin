3b267a7dee5356aa42bd4957556e099b2efe0f40
DanielRivasMD <danielrivasmd@gmail.com>
2021-04-13 17:10:12 +0200

    patch assembly list curator

--- src/collector/curateAssemblyList.sh
+++ src/collector/curateAssemblyList.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-awk 'BEGIN{FS = ","; OFS = " "} $2 ~/HiC/ && $3 !~/NA/ {for (ix = 1; ix <= NF; ix++) {printf $ix} printf "\n" }' $HOME/Factorem/Syncytin/data/assembly.list > $HOME/Factorem/Syncytin/data/CURATEDassembly.listt
+awk 'BEGIN{FS = ","; OFS = " "} $2 ~/HiC/ && $3 !~/NA/ {for (ix = 1; ix <= NF; ix++) {printf $ix} printf "\n" }' $HOME/Factorem/Syncytin/data/assembly.list > $HOME/Factorem/Syncytin/data/CURATEDassembly.list
 
