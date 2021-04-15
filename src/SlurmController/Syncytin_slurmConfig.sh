dc1fabc5566f04aefcd8da3efa6854480825c080
DanielRivasMD <danielrivasmd@gmail.com>
2021-04-15 16:52:45 +0200

    add curated assembly list

--- src/SlurmController/Syncytin_slurmConfig.sh
+++ src/SlurmController/Syncytin_slurmConfig.sh
@@ -5,5 +5,6 @@
 projectId=pawsey0263
 sourceFolder=${HOME}/Factorem/Syncytin
 reportFolder=${HOME}/Report/Syncytin
+curatedAssembly=${sourceFolder}/data/CURATEDassembly.list
 
 ################################################################################
