31cfa5293230fc9bc4087b936a56a2561184fc0e
DanielRivasMD <danielrivasmd@gmail.com>
2021-04-21 16:30:20 +0200

    add genome diamond slurm controller

---
+++ src/SlurmController/genomeDiamond.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# NOTE: runs on Magnus for parallelization
+source ${HOME}/Factorem/Syncytin/src/SlurmController/Syncytin_slurmConfig.sh
+jobId=SyncytinDiamond
+
+################################################################################
+
+sbatch \
+  --account ${projectId} \
+  --partition gpuq \
+  --job-name ${jobId} \
+  --output ${reportFolder}/${jobId}.out \
+  --rror ${reportFolder}/${jobId}.err \
+  --time 24:0:0 \
+  --nodes 1 \
+  --ntasks 4 \
+  --export sourceFolder=${sourceFolder} \
+  --wrap \
+    'bender GenomeDiamond --config ${sourceFolder}/src/diamond/genomeDiamond.toml \
+    --species $( sed -n "$SLURM_ARRAY_TASK_ID"p "${curatedAssembly}" | cut -d " " -f1 ) \
+    --assembly $( sed -n "$SLURM_ARRAY_TASK_ID"p "${curatedAssembly}" | cut -d " " -f2 )'
+
+    #'gzip -dc Crocuta_crocuta_HiC.fasta.gz | diamond blastx -d syncytinLibraryProt.dmnd -q - -o croCro.tsv'
+
+################################################################################e
