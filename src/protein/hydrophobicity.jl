3d572d486a510ae4a9d283f8a7b9fa74aa226d70
DanielRivasMD <danielrivasmd@gmail.com>
2021-09-11 13:17:18 +0200

    calculate hydrophobicity profile

--- src/protein/hydrophobicity.jl
+++ src/protein/hydrophobicity.jl
@@ -17,9 +17,21 @@ hydro = @chain begin
   rename!(_, ["AminoAcid", "OneLetterCode", "HydropathyScore"])
 end
 
+# cast columns
+hydro.AminoAcid = map(χ -> convert(String, χ), hydro.AminoAcid)
+hydro.OneLetterCode = map(χ -> AminoAcid(collect(χ)[end]), hydro.OneLetterCode)
+hydro.HydropathyScore = map(χ -> convert(Float64, χ), hydro.HydropathyScore)
+
 ################################################################################
 
 # load protein database
 synAr = syncytinReader("/Users/drivas/Factorem/Syncytin/data/syncytinDB/protein/CURATEDsyncytinLibrary.fasta")
+
+################################################################################
+
+score = Array{Float64, 1}(undef, 0)
+for aa in synAr[1] |> FASTX.sequence
+  push!(score, findfirst(χ -> χ == aa, hydro.OneLetterCode) |> π -> hydro[π, :HydropathyScore])
+end
 
 ################################################################################
