f47af57f057d266bb4104dca54f8cc377820e25b
DanielRivasMD <danielrivasmd@gmail.com>
2021-09-12 16:22:59 +0200

    add default values window slice with weight function

--- src/protein/hydrophobicity.jl
+++ src/protein/hydrophobicity.jl
@@ -10,6 +10,34 @@ include("/Users/drivas/Factorem/Syncytin/src/phylogeny/syncytinDB.jl");
 
 ################################################################################
 
+function calculateHydropathy(record, hydro)
+  # contrusct empty score vector
+  score = Array{Float64, 1}(undef, 0)
+  # collect aminoacid values
+  for aa in record |> FASTX.sequence
+    push!(score, findfirst(χ -> χ == aa, hydro.OneLetterCode) |> π -> hydro[π, :HydropathyScore])
+  end
+  return score
+end
+
+function windowSlide(score, bin = 9, weight = repeat([1.], 9))
+  # contruct vector
+  sliced = Array{Float64, 1}(undef, 0)
+  # iterate on vector
+  for ι ∈ 1:length(score)
+    # do not overshoot vector boundries
+    ω = ι + bin - 1
+    if ω > length(score)
+      break
+    end
+    # calculate bin average
+    push!(sliced, sum(score[ι:ω] .* weight) / bin)
+  end
+  return sliced
+end
+
+################################################################################
+
 # load hydrophobicity values
 hydro = @chain begin
   readdlm("src/protein/hydrophobicity.tsv")
@@ -28,33 +56,5 @@ end
 
 # load protein database
 synAr = syncytinReader("/Users/drivas/Factorem/Syncytin/data/syncytinDB/protein/CURATEDsyncytinLibrary.fasta")
-
-################################################################################
-
-function calculateHydropathy(record, hydro)
-  # contrusct empty score vector
-  score = Array{Float64, 1}(undef, 0)
-  # collect aminoacid values
-  for aa in record |> FASTX.sequence
-    push!(score, findfirst(χ -> χ == aa, hydro.OneLetterCode) |> π -> hydro[π, :HydropathyScore])
-  end
-  return score
-end
-
-function windowSlide(score, bin, weight)
-  # contruct vector
-  sliced = Array{Float64, 1}(undef, 0)
-  # iterate on vector
-  for ι ∈ 1:length(score)
-    # do not overshoot vector boundries
-    ω = ι + bin - 1
-    if ω > length(score)
-      break
-    end
-    # calculate bin average
-    push!(sliced, sum(score[ι:ω] .* weight) / bin)
-  end
-  return sliced
-end
 
 ################################################################################
