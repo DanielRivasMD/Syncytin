b4255a8950ea40462f00225a75b5b3133808dbfc
DanielRivasMD <danielrivasmd@gmail.com>
2021-09-13 11:56:08 +0200

    add multiple sequence alignment

---
+++ src/phylogeny/syncytinMSA.R
@@ -0,0 +1,26 @@
+################################################################################
+
+library(tidyverse)
+library(msa)
+
+################################################################################
+
+# declare syncytin library
+synFile <- 'data/syncytinDB/protein/CURATEDsyncytinLibrary.fasta'
+
+################################################################################
+
+# load syncytin library
+synSeq <- readAAStringSet(synFile)
+
+################################################################################
+
+# calculate multiple sequence alignment
+synMSA <- msa(synSeq, 'ClustalW')
+
+################################################################################
+
+# print multiple sequence alignment
+msaPrettyPrint(synMSA, output = 'pdf', showLogo = 'none', askForOverwrite = FALSE)
+
+################################################################################
