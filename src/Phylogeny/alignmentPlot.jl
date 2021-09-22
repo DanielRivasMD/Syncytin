e4c61ca01ceabad0eeddf71b64e13728623ca8e8
DanielRivasMD <danielrivasmd@gmail.com>
2021-09-22 21:48:28 +0200

    patch alignment plot

--- src/Phylogeny/alignmentPlot.jl
+++ src/Phylogeny/alignmentPlot.jl
@@ -15,6 +15,7 @@ end;
 ################################################################################
 
 # TODO: dependes on genomicPositions. perhaps write to csv
+# TODO: dependes on taxonomist. perhaps also write to csv
 diamondHits = freqtable(positionDf.Species)
 toPlotDf = taxonomyDf[:, [:Species, :Order]]
 
@@ -23,7 +24,7 @@ insertcols!(toPlotDf, :DiamondHits => 0)
 for ρ ∈ eachrow(toPlotDf)
   ixDc = diamondHits.dicts[1]
   if haskey(ixDc, ρ.Species)
-    ρ.DiamondHits = diamondHits[ixDc[r.Species]]
+    ρ.DiamondHits = diamondHits[ixDc[ρ.Species]]
   end
 end
 
