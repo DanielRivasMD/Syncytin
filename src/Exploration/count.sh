e0cb98bf21ddc7d05dc9cea0f2599fd16d07ed96
DanielRivasMD <danielrivasmd@gmail.com>
2021-10-01 08:45:45 +0200

    collect diamond counts

---
+++ src/Exploration/count.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+################################################################################
+
+# declarations
+projDir=$HOME/Factorem/Syncytin
+dataDir=${projDir}/data
+diamond=${dataDir}/diamondOutput
+stats=${diamond}/stats
+
+################################################################################
+
+cd ${diamond}
+
+################################################################################
+
+# create stats file
+echo "Alignment,Count" > ${stats}/diamond.csv
+
+################################################################################
+
+# total
+total=$( $(which fd) --type=directory --max-depth=1 | wc | awk '{print $1 - 1}' )
+
+# filtered
+filtered=$( $(which fd) filtered | wc | awk '{print $1}' )
+
+# no hits
+noHits=$( $(which fd) --size=0B tsv | wc | awk '{print $1}' )
+
+# print
+echo $filtered | awk '{print "Filtered," $0}' >> ${stats}/diamond.csv
+echo $(( $total - ($filtered + $noHits) )) | awk '{print "NoFiltered," $0}' >> ${stats}/diamond.csv
+echo $noHits | awk '{print "NoHits," $0}' >> ${stats}/diamond.csv
+
+################################################################################
+
+# return directory
+cd - > /dev/null
+
+################################################################################
