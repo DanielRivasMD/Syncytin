b1652d402ced133dbf0844349a3a381bf53f4556
DanielRivasMD <danielrivasmd@gmail.com>
2021-10-17 13:36:27 +0200

    update curate assembly list

--- src/Exploration/curateAssemblyList.sh
+++ src/Exploration/curateAssemblyList.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-awk 'BEGIN{FS = ","; OFS = " "} $2 ~/HiC/ && $3 !~/NA/ {for (ix = 1; ix <= NF; ix++) {printf $ix} printf "\n" }' ${HOME}/Factorem/Syncytin/data/phylogeny/assembly.list > ${HOME}/Factorem/Syncytin/data/phylogeny/CURATEDassembly.list
+################################################################################
+
+# config
+source ${HOME}/Factorem/Syncytin/src/Config/syncytinConfig.sh
+
+################################################################################
+
+# select HiC & annotated assemblies
+awk '
+  BEGIN{
+    FS = ",";
+    OFS = " "
+  }
+
+  {
+    $2 ~/HiC/ && $3 !~/NA/ {
+      for (ix = 1; ix <= NF; ix++) {
+        printf $ix
+      }
+      printf "\n"
+    }
+  }
+' ${phylogeny}/assembly.list > ${phylogeny}/CURATEDassembly.list
+
+################################################################################
