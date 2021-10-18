ff72e90173deb9c09acbd818bce3f2937f71725f
DanielRivasMD <danielrivasmd@gmail.com>
2021-10-18 20:43:55 +0200

    patch slurm controller retrieve assemblies

--- src/SlurmController/assemblies.sh
+++ src/SlurmController/assemblies.sh
@@ -14,7 +14,7 @@ slurmJobId=$( sbatch \
     --time 24:00:00 \
     --nodes 1 \
     --ntasks 1 \
-  ${sourceFolder}/src/Exploration/assemblies.sh )
+  ${sourceFolder}/src/Exploration/retrieveAssemblies.sh )
 
 echo ${slurmJobId}
 
