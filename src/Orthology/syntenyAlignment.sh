7de9d5a1662e5cc8a68697c1c3819837bd6275b5
DanielRivasMD <danielrivasmd@gmail.com>
2021-10-22 19:13:04 +0200

    update synteny alignment
    synteny alignment is the protocol to extract
    syntenic anchor from annotations
    add slurm controller as parallel
    update shell script using bender assembly synteny
    deprecate synteny annotation retrieve go

---
+++ src/Orthology/syntenyAlignment.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# set -euo pipefail
+
+################################################################################
+
+# config
+source ${HOME}/Factorem/Syncytin/src/Config/syncytinConfig.sh
+
+################################################################################
+
+# capture argument
+if [[ $# -eq 0 ]]
+then
+  echo "Error: download directory must be provided as an input argument."
+  exit 1
+else
+  # argument
+  align=$1
+fi
+
+################################################################################
+
+# collect species name
+spp=$( awk -v align=$align 'BEGIN{FS = ","} {if ( $3 == align ) print $1}' "${wasabi}/filter/assemblyList.csv" )
+
+# write candidate loci
+awk -v spp=$spp 'BEGIN{FS = ","} {if ($8 == spp) print $1, $2, $6}' ${phylogeny}/lociDf.csv > ${phylogeny}/${spp}
+
+# decompress assembly & keep compressed
+gzip --decompress --stdout ${annotation}/${align} > ${annotation}/${align/.gz/}
+
+# collect annotations around candidate loci
+while read scaffold start end
+do
+
+  # annotation
+  bender assembly synteny \
+    --inDir ${annotation} \
+    --species ${align/.gz/} \
+    --scaffold ${scaffold} \
+    --start ${start} \
+    --end ${end} \
+    --hood 100000
+
+done < ${phylogeny}/${spp}
+
+# remove decompressed annotation
+rm ${annotation}/${align/.gz/}
+
+# remove candidate loci
+rm ${phylogeny}/${spp}
+
+################################################################################
