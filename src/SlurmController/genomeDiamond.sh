b61a9db26a4538f9c6d57ec26615e802f14009f7
DanielRivasMD <danielrivasmd@gmail.com>
2021-10-31 09:55:38 +0100

    incorporate ncbi assembly
    add slurm controller genome diamond ncbi run
    add synctyin config slurm assembly paths
    deprecate genome diamond toml indir path
    export indir path from syncytin config slurm
    pass indir as wrap variable
    update ncbi list as csv similar to assembly list

--- src/SlurmController/genomeDiamond.sh
+++ src/SlurmController/genomeDiamond.sh
@@ -6,24 +6,55 @@
 source "${HOME}/Factorem/Syncytin/src/SlurmController/syncytinConfigSLURM.sh"
 
 ################################################################################
+# DNAzoo
+################################################################################
+
 
 # zeus
 sbatch \
   --account "${projectId}" \
   --clusters zeus \
+  --partition highmemq \
   --job-name SyncytinDiamond \
   --output "${reportFolder}/%x_%j_%a.out" \
   --error "${reportFolder}/%x_%j_%a.err" \
   --time 12:0:0 \
   --nodes 1 \
-  --ntasks 16 \
-  --export sourceFolder="${sourceFolder}",assemblyList="${assemblyList}" \
+  --export sourceFolder="${sourceFolder}",DNAzooDir="${DNAzooDir}",assemblyList="${assemblyList}" \
   --array 1-$( awk 'END{print NR}' "${assemblyList}" ) \
   --wrap \
   'bender assembly search diamond \
   --configPath "${sourceFolder}/src/Exploration/diamond/" \
   --configFile "genomeDiamond.toml" \
+  --inDir "${DNAzooDir}" \
   --species $( sed -n "$SLURM_ARRAY_TASK_ID"p "${assemblyList}" | cut -d "," -f 1 ) \
   --assembly $( sed -n "$SLURM_ARRAY_TASK_ID"p "${assemblyList}" | cut -d "," -f 2 )'
 
+
 ################################################################################
+# NCBI
+################################################################################
+
+# zeus
+sbatch \
+  --account "${projectId}" \
+  --clusters zeus \
+  --partition highmemq \
+  --job-name SyncytinDiamond \
+  --output "${reportFolder}/%x_%j_%a.out" \
+  --error "${reportFolder}/%x_%j_%a.err" \
+  --time 12:0:0 \
+  --nodes 1 \
+  --export sourceFolder="${sourceFolder}",ncbiDir="${ncbiDir}",ncbiList="${ncbiList}" \
+  --array 1-$( awk 'END{print NR}' "${ncbiList}" ) \
+  --wrap \
+  'bender assembly search diamond \
+  --configPath "${sourceFolder}/src/Exploration/diamond/" \
+  --configFile "genomeDiamond.toml" \
+  --inDir "${ncbiDir}" \
+  --species $( sed -n "$SLURM_ARRAY_TASK_ID"p "${ncbiList}" | cut -d "," -f 1 ) \
+  --assembly $( sed -n "$SLURM_ARRAY_TASK_ID"p "${ncbiList}" | cut -d "," -f 2 )'
+
+################################################################################
+
+# TODO: update to extract long sequences & decompress
--- src/SlurmController/syncytinConfigSLURM.sh
+++ src/SlurmController/syncytinConfigSLURM.sh
@@ -2,10 +2,17 @@
 
 ################################################################################
 
-# declarations
+# project
 projectId="pawsey0263"
 sourceFolder="${HOME}/Factorem/Syncytin"
 reportFolder="${HOME}/Report/Syncytin"
+
+# assembly list
 assemblyList="${sourceFolder}/list/assemblyList.csv"
+ncbiList="${sourceFolder}/list/ncbiList.csv"
+
+# assembly directory
+DNAzooDir="/scratch/pawsey0263/drivas/Factorem/Syncytin/data/DNAzooSource"
+ncbiDir="/scratch/pawsey0263/drivas/Factorem/Syncytin/data/ncbiAssemblyAnnotationSource"
 
 ################################################################################
