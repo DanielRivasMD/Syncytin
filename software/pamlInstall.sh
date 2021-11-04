760d942a6ef949b966c48c86f7a858f9759cb949
DanielRivasMD <danielrivasmd@gmail.com>
2021-11-04 17:23:02 +0100

    document third party software install
    add paml
    deprecate software gitnore

---
+++ software/pamlInstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+################################################################################
+
+# download
+wget http://abacus.gene.ucl.ac.uk/software/paml4.8a.macosx.tgz
+
+################################################################################
+
+# expand tarball
+tar -xvf paml4.8a.macosx.tgz
+
+# enter directory
+cd paml4.8
+
+# compile
+cd src
+make -f Makefile
+
+# relocate executables
+mv baseml basemlg codeml pamp evolver yn00 chi2 infinitesites mcmctree ../bin
+
+################################################################################
