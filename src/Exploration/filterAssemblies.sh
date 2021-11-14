4ec04354d432948774205af9163e383750e603d0
DanielRivasMD <danielrivasmd@gmail.com>
2021-11-14 13:26:35 +0100

    patch dna zoo assembly list
    add condition to eliminate golden hamster short read assembly
    update dna zoo list csv file

--- src/Exploration/filterAssemblies.sh
+++ src/Exploration/filterAssemblies.sh
@@ -17,39 +17,46 @@ fi
 # filter
 for assembly in $( $(which exa) "${wasabiDir}/raw" )
 do
-  awk \
-    -v assemblySpp="${assembly/.csv/}" \
-    '
-    BEGIN{
-      FS = ",";
-      OFS = ","
-      RS = "\r\n"
-    }
 
-    {
-      if ( /README/ ) {
-        readmeLink = $2;
+  if [[ "${assembly}" != "Mesocricetus_auratus__golden_hamster_wtdbg2.shortReadsPolished.csv" ]]
+  then
+
+    awk \
+      -v assemblySpp="${assembly/.csv/}" \
+      '
+      BEGIN{
+        FS = ",";
+        OFS = ","
+        RS = "\r\n"
       }
 
-      if ( /HiC.fasta.gz/ ) {
-        assembly++;
-        assemblyID = $1;
-        assemblyLink = $2;
+      {
+        if ( /README/ ) {
+          readmeLink = $2;
+        }
+
+        if ( /HiC.fasta.gz/ ) {
+          assembly++;
+          assemblyID = $1;
+          assemblyLink = $2;
+        }
+
+        if ( /fasta_v2.functional.gff3.gz/ ) {
+          annotation++;
+          annotationID = $1;
+          annotationLink = $2;
+        }
       }
 
-      if ( /fasta_v2.functional.gff3.gz/ ) {
-        annotation++;
-        annotationID = $1;
-        annotationLink = $2;
+      END{
+        if ( annotation > 0 && assembly > 0 ) {
+          print assemblySpp, assemblyID, annotationID, readmeLink, assemblyLink, annotationLink;
+        }
       }
-    }
+    ' "${wasabiDir}/raw/${assembly}" >> "${listDir}/DNAzooList.csv"
 
-    END{
-      if ( annotation > 0 && assembly > 0 ) {
-        print assemblySpp, assemblyID, annotationID, readmeLink, assemblyLink, annotationLink;
-      }
-    }
-  ' "${wasabiDir}/raw/${assembly}" >> "${listDir}/DNAzooList.csv"
+  fi
+
 done
 
 ################################################################################
