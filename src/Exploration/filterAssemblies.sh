2a4fc0b9fcb304ca69bdb83fee61ea5205b7ed1c
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-14 15:11:27 +0800

    deprecate awk filtering & use bender assembly filter

--- src/Exploration/filterAssemblies.sh
+++ src/Exploration/filterAssemblies.sh
@@ -21,39 +21,7 @@ do
   if [[ "${assembly}" != "Mesocricetus_auratus__golden_hamster_wtdbg2.shortReadsPolished.csv" ]]
   then
 
-    awk \
-      -v assemblySpp="${assembly/.csv/}" \
-      '
-      BEGIN{
-        FS = ",";
-        OFS = ","
-        RS = "\r\n"
-      }
-
-      {
-        if ( /README/ ) {
-          readmeLink = $2;
-        }
-
-        if ( /HiC.fasta.gz/ ) {
-          assembly++;
-          assemblyID = $1;
-          assemblyLink = $2;
-        }
-
-        if ( /fasta_v2.functional.gff3.gz/ ) {
-          annotation++;
-          annotationID = $1;
-          annotationLink = $2;
-        }
-      }
-
-      END{
-        if ( annotation > 0 && assembly > 0 ) {
-          print assemblySpp, assemblyID, annotationID, readmeLink, assemblyLink, annotationLink;
-        }
-      }
-    ' "${wasabiDir}/raw/${assembly}" >> "${listDir}/DNAzooList.csv"
+    bender assembly filter --inDir "${wasabiDir}/raw/" --species "${assembly}" >> "${listDir}/DNAzooList.csv"
 
   fi
 
