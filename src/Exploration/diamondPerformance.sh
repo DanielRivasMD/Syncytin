2eb295727e61410af5af74c5f798364603100ac3
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-20 04:40:53 +0200

    add diamond performance

---
+++ src/Exploration/diamondPerformance.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -euo pipefail
+
+####################################################################################################
+
+# config
+source "${HOME}/Factorem/Syncytin/src/Config/syncytinConfig.sh"
+
+####################################################################################################
+
+# define assembly
+species=$(sed -n "$SLURM_ARRAY_TASK_ID"p "${assemblyList}" | cut -d "," -f 1)
+assembly=$(sed -n "$SLURM_ARRAY_TASK_ID"p "${assemblyList}" | cut -d "," -f 2)
+
+####################################################################################################
+
+# # decompress assembly & keep compressed
+# gzip --decompress --stdout "${assemblyDir}/${assembly}" > "${dataDir}/tmp/${assembly/.gz/}"
+
+# similarity search
+bender assembly search diamond \
+  --configPath "${sourceFolder}/src/Exploration/config/" \
+  --configFile "diamond.toml" \
+  --inDir "${dataDir}/tmp" \
+  --species "${species}" \
+  --blockSize "${bk}" \
+  --indexChunks "${ix}" \
+  --assembly "${assembly/.gz/}"
+
+# # remove decompressed assembly
+# rm "${dataDir}/tmp/${assembly/.gz/}"
+
+####################################################################################################
