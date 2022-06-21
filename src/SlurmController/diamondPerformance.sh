aefad5ccc7c9b75413f2cb90bb59cdd2b6075c24
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-21 05:37:23 +0200

    test increased index chunks

--- src/SlurmController/diamondPerformance.sh
+++ src/SlurmController/diamondPerformance.sh
@@ -12,9 +12,9 @@ source "${HOME}/Factorem/Syncytin/src/SlurmController/syncytinConfigSLURM.sh"
 # parameters to test
 # blockSize=( 0.2 0.3 0.4 0.5 1.0 1.5 2.0 )
 blockSize=( 0.4 )
-indexChunksMin=10
-indexChunksMax=100
-indexChunksStep=10
+indexChunksMin=100
+indexChunksMax=1000
+indexChunksStep=100
 
 # zeus
 for bk in "${blockSize[@]}"
@@ -42,27 +42,27 @@ done
 # NCBI
 ####################################################################################################
 
-# zeus
-for bk in "${blockSize[@]}"
-do
-  for ix in {${indexChunksMin}..${indexChunksMax}..${indexChunksStep}}
-  do
+# # zeus
+# for bk in "${blockSize[@]}"
+# do
+#   for ix in {${indexChunksMin}..${indexChunksMax}..${indexChunksStep}}
+#   do
 
-    sbatch \
-      --account "${projectId}" \
-      --clusters zeus \
-      --partition highmemq \
-      --job-name Performance \
-      --output "${reportFolder}/%x_${bk}_${ix}_%j_%a.out" \
-      --error "${reportFolder}/%x_${bk}_${ix}_%j_%a.err" \
-      --time 24:0:0 \
-      --nodes 1 \
-      --export sourceFolder="${sourceFolder}",assemblyDir="${ncbiDir}",assemblyList="${ncbiList}",bk="${bk}",ix="${ix}" \
-      --array 1 \
-      "${sourceFolder}/src/Exploration/diamondPerformance.sh"
+#     sbatch \
+#       --account "${projectId}" \
+#       --clusters zeus \
+#       --partition highmemq \
+#       --job-name Performance \
+#       --output "${reportFolder}/%x_${bk}_${ix}_%j_%a.out" \
+#       --error "${reportFolder}/%x_${bk}_${ix}_%j_%a.err" \
+#       --time 24:0:0 \
+#       --nodes 1 \
+#       --export sourceFolder="${sourceFolder}",assemblyDir="${ncbiDir}",assemblyList="${ncbiList}",bk="${bk}",ix="${ix}" \
+#       --array 1 \
+#       "${sourceFolder}/src/Exploration/diamondPerformance.sh"
 
-  done
-done
+#   done
+# done
 
 ####################################################################################################
 
