7def487faaf64fe1ada64f1dd15a8cb7b573921b
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-22 06:30:20 +0200

    add segregate scaffold script

--- src/Exploration/diamondSearch.sh
+++ src/Exploration/diamondSearch.sh
@@ -17,15 +17,32 @@ assembly=$(sed -n "$SLURM_ARRAY_TASK_ID"p "${assemblyList}" | cut -d "," -f 2)
 # decompress assembly & keep compressed
 gzip --decompress --stdout "${assemblyDir}/${assembly}" > "${dataDir}/tmp/${assembly/.gz/}"
 
-# similarity search
-bender assembly search diamond \
-  --configPath "${sourceFolder}/src/Exploration/config/" \
-  --configFile "diamond.toml" \
-  --inDir "${dataDir}/tmp" \
-  --species "${species}" \
-  --assembly "${assembly/.gz/}"
+# create directory to hold scaffolds
+mkdir "${dataDir}/tmp/${assembly/.fasta.gz/}"
 
-# remove decompressed assembly
+# segregate scaffolds
+bender fasta segregate \
+  --inDir "${dataDir}/tmp" \
+  --outDir "${dataDir}/tmp/${assembly/.fasta.gz/}" \
+  --fasta "${assembly/.gz/}"
+
+# iterate on scaffolds
+for scaffold in $(command ls "${dataDir}/tmp/${assembly/.fasta.gz/}")
+do
+
+  # similarity search
+  bender assembly search diamond \
+    --configPath "${sourceFolder}/src/Exploration/config/" \
+    --configFile "diamond.toml" \
+    --inDir "${dataDir}/tmp/${assembly/.fasta.gz/}" \
+    --species "${species}" \
+    --assembly "${scaffold}"
+
+done
+
+# remove decompressed assembly & segregated files
 rm "${dataDir}/tmp/${assembly/.gz/}"
+rm "${dataDir}/tmp/${assembly/.fasta.gz/}/"*
+rmdir "${dataDir}/tmp/${assembly/.fasta.gz/}"
 
 ####################################################################################################
