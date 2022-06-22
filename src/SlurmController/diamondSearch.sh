fa863a10253ebc018ed3e7cb321ae827cff7147e
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-22 05:12:57 +0200

    add segregated scaffolds test

--- src/SlurmController/diamondSearch.sh
+++ src/SlurmController/diamondSearch.sh
@@ -17,13 +17,13 @@ sbatch \
   --job-name SyncytinDiamond \
   --output "${reportFolder}/%x_%j_%a.out" \
   --error "${reportFolder}/%x_%j_%a.err" \
-  --time 12:0:0 \
+  --time 24:0:0 \
   --nodes 1 \
   --export sourceFolder="${sourceFolder}",assemblyDir="${DNAzooDir}",assemblyList="${DNAzooList}" \
-  --array 68,90,92,106,120,126,132,133,142,144,145,156 \
-  --array 1-$(awk 'END{print NR}' "${DNAzooList}") \
+  --array 103 \
   "${sourceFolder}/src/Exploration/diamondSearch.sh"
-# patch. run only failed assemblies
+
+  # --array 1-$( awk 'END{print NR}' "${DNAzooList}" ) \
 
 ####################################################################################################
 # NCBI
