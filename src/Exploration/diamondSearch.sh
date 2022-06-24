38a4222343c3380ca3632606fa1150c021e96754
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-24 07:07:11 +0200

    check directory & create. redirect raw diamond output

--- src/Exploration/diamondSearch.sh
+++ src/Exploration/diamondSearch.sh
@@ -18,13 +18,22 @@ assembly=$(sed -n "$SLURM_ARRAY_TASK_ID"p "${assemblyList}" | cut -d "," -f 2)
 gzip --decompress --stdout "${assemblyDir}/${assembly}" > "${dataDir}/tmp/${assembly/.gz/}"
 
 # create directory to hold scaffolds
-mkdir "${dataDir}/tmp/${species}"
+if [[ ! -d "${dataDir}/tmp/${species}" ]]
+then
+  mkdir "${dataDir}/tmp/${species}"
+fi
 
 # segregate scaffolds
 bender fasta segregate \
   --inDir "${dataDir}/tmp" \
   --outDir "${dataDir}/tmp/${species}" \
   --fasta "${assembly}"
+
+# create directory to hold diamond output
+if [[ ! -d "${diamondDir}/raw/${species}" ]]
+then
+  mkdir "${diamondDir}/raw/${species}"
+fi
 
 # iterate on scaffolds
 for scaffold in $(command ls "${dataDir}/tmp/${species}")
