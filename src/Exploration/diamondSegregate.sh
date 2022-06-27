71710f92fa92d0c86c62c9dc60851d05297be8e8
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-27 04:28:35 +0200

    add diamond segregate script

---
+++ src/Exploration/diamondSegregate.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -euo pipefail
+
+####################################################################################################
+
+# config
+source "${HOME}/Factorem/Syncytin/src/Config/syncytinConfig.sh"
+
+####################################################################################################
+
+# define assembly
+species=$(sed -n "$SLURM_ARRAY_TASK_ID"p "${assemblyList}" | cut -d "," -f 1)
+assembly=$(sed -n "$SLURM_ARRAY_TASK_ID"p "${assemblyList}" | cut -d "," -f 2)
+
+####################################################################################################
+
+# decompress assembly & keep compressed
+gzip --decompress --stdout "${assemblyDir}/${assembly}" > "${dataDir}/tmp/${assembly/.gz/}"
+
+# create directory to hold scaffolds
+if [[ ! -d "${dataDir}/tmp/${species}" ]]
+then
+  mkdir "${dataDir}/tmp/${species}"
+fi
+
+# segregate scaffolds
+bender fasta segregate \
+  --inDir "${dataDir}/tmp" \
+  --outDir "${dataDir}/tmp/${species}" \
+  --fasta "${assembly}"
+
+# remove decompressed assembly
+rm "${dataDir}/tmp/${assembly/.gz/}"
+
+####################################################################################################
