725d53e172bf107a1cc186f5afdcf1aa51c261b7
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-27 04:28:23 +0200

    refactor scaffold segregation

--- src/Exploration/diamondSearch.sh
+++ src/Exploration/diamondSearch.sh
@@ -14,21 +14,6 @@ assembly=$(sed -n "$SLURM_ARRAY_TASK_ID"p "${assemblyList}" | cut -d "," -f 2)
 
 ####################################################################################################
 
-# decompress assembly & keep compressed
-gzip --decompress --stdout "${assemblyDir}/${assembly}" > "${dataDir}/tmp/${assembly/.gz/}"
-
-# create directory to hold scaffolds
-if [[ ! -d "${dataDir}/tmp/${species}" ]]
-then
-  mkdir "${dataDir}/tmp/${species}"
-fi
-
-# segregate scaffolds
-bender fasta segregate \
-  --inDir "${dataDir}/tmp" \
-  --outDir "${dataDir}/tmp/${species}" \
-  --fasta "${assembly}"
-
 # create directory to hold diamond output
 if [[ ! -d "${diamondDir}/raw/${species}" ]]
 then
