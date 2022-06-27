8769179f1f6cda932b2e43f1557a114cc88fbfd3
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-27 05:54:26 +0200

    overload job parallelization at magnus

--- src/SlurmController/diamondSegregate.sh
+++ src/SlurmController/diamondSegregate.sh
@@ -20,21 +20,7 @@ sbatch \
   --time 24:0:0 \
   --nodes 1 \
   --export sourceFolder="${sourceFolder}",assemblyDir="${DNAzooDir}",assemblyList="${DNAzooList}" \
-  --array 1-100 \
-  "${sourceFolder}/src/Exploration/diamondSegregate.sh"
-
-# magnus
-sbatch \
-  --account "${projectId}" \
-  --clusters magnus \
-  --partition workq \
-  --job-name SyncytinDiamond \
-  --output "${reportFolder}/%x_%j_%a.out" \
-  --error "${reportFolder}/%x_%j_%a.err" \
-  --time 24:0:0 \
-  --nodes 1 \
-  --export sourceFolder="${sourceFolder}",assemblyDir="${DNAzooDir}",assemblyList="${DNAzooList}" \
-  --array 101-$(awk 'END{print NR}' "${DNAzooList}") \
+  --array 1-$(awk 'END{print NR}' "${DNAzooList}") \
   "${sourceFolder}/src/Exploration/diamondSegregate.sh"
 
 ####################################################################################################
