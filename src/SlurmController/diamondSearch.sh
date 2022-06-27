147435256569b11318aba2e9ebbb2286bb3707c3
DanielRivasMD <danielrivasmd@gmail.com>
2022-06-27 06:42:24 +0200

    use magnus for diamond search

--- src/SlurmController/diamondSearch.sh
+++ src/SlurmController/diamondSearch.sh
@@ -9,43 +9,29 @@ source "${HOME}/Factorem/Syncytin/src/SlurmController/syncytinConfigSLURM.sh"
 # DNAzoo
 ####################################################################################################
 
-# zeus
+# magnus
 sbatch \
   --account "${projectId}" \
-  --clusters zeus \
-  --partition highmemq \
+  --clusters magnus \
+  --partition workq \
   --job-name SyncytinDiamond \
   --output "${reportFolder}/%x_%j_%a.out" \
   --error "${reportFolder}/%x_%j_%a.err" \
   --time 24:0:0 \
   --nodes 1 \
   --export sourceFolder="${sourceFolder}",assemblyDir="${DNAzooDir}",assemblyList="${DNAzooList}" \
-  --array 1-100 \
-  "${sourceFolder}/src/Exploration/diamondSearch.sh"
-
-# zeus
-sbatch \
-  --account "${projectId}" \
-  --clusters zeus \
-  --partition highmemq \
-  --job-name SyncytinDiamond \
-  --output "${reportFolder}/%x_%j_%a.out" \
-  --error "${reportFolder}/%x_%j_%a.err" \
-  --time 24:0:0 \
-  --nodes 1 \
-  --export sourceFolder="${sourceFolder}",assemblyDir="${DNAzooDir}",assemblyList="${DNAzooList}" \
-  --array 101-$(awk 'END{print NR}' "${DNAzooList}") \
+  --array 1-$(awk 'END{print NR}' "${DNAzooList}") \
   "${sourceFolder}/src/Exploration/diamondSearch.sh"
 
 ####################################################################################################
 # NCBI
 ####################################################################################################
 
-# zeus
+# magnus
 sbatch \
   --account "${projectId}" \
-  --clusters zeus \
-  --partition highmemq \
+  --clusters magnus \
+  --partition workq \
   --job-name SyncytinDiamond \
   --output "${reportFolder}/%x_%j_%a.out" \
   --error "${reportFolder}/%x_%j_%a.err" \
