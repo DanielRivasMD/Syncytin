4ecd15729a85f988ad95968cbfde3a99f0977437
DanielRivasMD <danielrivasmd@gmail.com>
2022-12-12 10:01:15 -0600

    update decomissioned magnus

--- .just.sh
+++ .just.sh
@@ -3,7 +3,7 @@
 #################################################################################
 
 # remote address
-pawseyID="drivas@magnus.pawsey.org.au"
+pawseyID="drivas@topaz.pawsey.org.au"
 pawseyProj="/scratch/pawsey0263/drivas"
 projRemote="${pawseyProj}/Factorem/Syncytin"
 reportRemote="${pawseyProj}/Report/Syncytin"
